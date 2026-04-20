ebf5e449f6ef16dd054cfea938fec58a522bdbbe
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2026-04-20 12:03:21 +0600

    added barcode scanning

---
+++ lib/views/barcode_window.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:pharmaciano/app/pos_providers.dart';
+
+class BarcodeWindow extends ConsumerStatefulWidget {
+  BarcodeWindow({Key? key}) : super(key: key);
+
+  @override
+  _BarcodeWindowState createState() => _BarcodeWindowState();
+}
+
+class _BarcodeWindowState extends ConsumerState<BarcodeWindow> {
+  late MobileScannerController _controller;
+  bool flashOn = false;
+  @override
+  void initState() {
+    super.initState();
+    _controller = MobileScannerController(
+      torchEnabled: flashOn,
+      autoZoom: true,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("barcode"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                flashOn = !flashOn;
+                _controller.toggleTorch();
+              });
+            },
+            icon: Icon(flashOn ? Icons.flash_on_sharp : Icons.flash_off_sharp),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          MobileScanner(
+            controller: _controller,
+
+            onDetect: (capture) {
+              String? result = capture.barcodes.first.rawValue;
+              if (result != null) {
+                ref
+                    .read(barcodeProvider.notifier)
+                    .update(capture.barcodes.first.rawValue!);
+                    Navigator.pop(context);
+              }
+              if (kDebugMode) {
+                print(capture.barcodes.first.rawValue);
+              }
+            },
+          ),
+          BarcodeOverlay(
+            boxFit: BoxFit.cover,
+            controller: _controller, // Same controller used above
+            color: Colors.green, // Box color
+            style: PaintingStyle.stroke, // Just the outline
+          ),
+        ],
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import file_saver
 import flutter_secure_storage_macos
 import mobile_scanner
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
